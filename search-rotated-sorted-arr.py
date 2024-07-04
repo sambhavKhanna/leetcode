b5f6f88b3024ae299d9b808363146040b5ef3567
sambhavKhanna <khsambhav@gmail.com>
2024-07-04 23:46:27 +0300

    July 4

---
+++ search-rotated-sorted-arr.py
@@ -0,0 +1,40 @@
+"""
+There is an integer array nums sorted in ascending order (with distinct values).
+
+Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+
+Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+You must write an algorithm with O(log n) runtime complexity.
+"""
+
+def search(self, nums: List[int], target: int) -> int:
+    k = len(nums)
+    l = 0
+    r = len(nums) -1
+    def search_(low = 0, high = len(nums) -1):
+        l = low
+        r = high
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            elif nums[m] < target:
+                l = m +1
+            else:
+                r = m -1
+        return -1
+
+    while l <= r:
+        m = (l + r) // 2
+        if nums[m] < nums[0]:
+            k = min(m, k)
+            r = m -1
+        else:
+            l = m +1
+
+    if target <= nums[-1]:
+        if k == len(nums):
+            k = 0
+        return search_(low=k)
+    return search_(high=k-1)
